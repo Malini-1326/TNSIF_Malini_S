b53ff947b1cebb05eb2937a56c713aefa0e30656
Malini-1326 <malinism2005@gmail.com>
2025-10-08 14:13:35 +0530

    commit 2

---
+++ Tnsif_java_Malini/src/com/tnsif/dayfive/interfacedemo/FunctionalDemo.java
@@ -0,0 +1,15 @@
+package com.tnsif.dayfive;
+
+public class FunctionalDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Lambda Expression
+				GreetingDemo gd = () -> 
+				System.out.println("Hello Everyone, Welcome to the Java Session");
+				
+				gd.greet();
+
+	}
+
+}
---
+++ Tnsif_java_Malini/src/com/tnsif/dayfive/interfacedemo/GreetingDemo.java
@@ -0,0 +1,5 @@
+package com.tnsif.dayfive;
+@FunctionalInterface
+public class GreetingDemo {
+	void greet(); //used for declaration
+}
